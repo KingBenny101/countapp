1728d9779696ca467647b3de5c3558380360568a
KingBenny101 <47111832+KingBenny101@users.noreply.github.com>
2024-11-29 13:13:35 +0530

    Fixed overflow in add_counter_page

--- lib/screens/add_counter_page.dart
+++ lib/screens/add_counter_page.dart
@@ -5,7 +5,6 @@ import 'package:provider/provider.dart';
 import '../providers/counter_provider.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-
 class AddCounterPage extends StatefulWidget {
   const AddCounterPage({super.key});
 
@@ -26,74 +25,76 @@ class AddCounterPageState extends State<AddCounterPage> {
       appBar: AppBar(title: const Text('Create a New Counter')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Counter Name',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.edit),
-                ),
-                onChanged: (value) {
-                  _name = value;
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter a name';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Step Size',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.add_circle_outline_rounded),
-                ),
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly,
-                ],
-                onChanged: (value) {
-                  _stepSize = int.tryParse(value) ?? 1;
-                },
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Initial Count',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.looks_one),
-                ),
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly,
-                ],
-                onChanged: (value) {
-                  _initialCount = int.tryParse(value) ?? 0;
-                },
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Type: ${_isIncrement ? 'Increment' : 'Decrement'}',
-                      style: const TextStyle(fontSize: 16)),
-                  Switch(
-                    value: _isIncrement,
-                    onChanged: (value) {
-                      setState(() {
-                        _isIncrement = value;
-                      });
-                    },
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Counter Name',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.edit),
                   ),
-                ],
-              ),
-            ],
+                  onChanged: (value) {
+                    _name = value;
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a name';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Step Size',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.add_circle_outline_rounded),
+                  ),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly,
+                  ],
+                  onChanged: (value) {
+                    _stepSize = int.tryParse(value) ?? 1;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Initial Count',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.looks_one),
+                  ),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly,
+                  ],
+                  onChanged: (value) {
+                    _initialCount = int.tryParse(value) ?? 0;
+                  },
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Type: ${_isIncrement ? 'Increment' : 'Decrement'}',
+                        style: const TextStyle(fontSize: 16)),
+                    Switch(
+                      value: _isIncrement,
+                      onChanged: (value) {
+                        setState(() {
+                          _isIncrement = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -112,9 +113,9 @@ class AddCounterPageState extends State<AddCounterPage> {
             Navigator.pop(context);
             Fluttertoast.showToast(
               msg: "Counter Added Successfully!",
-              toastLength: Toast.LENGTH_SHORT, 
-              gravity: ToastGravity.BOTTOM, 
-              timeInSecForIosWeb: 1, 
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.BOTTOM,
+              timeInSecForIosWeb: 1,
               backgroundColor: Colors.green,
               textColor: Colors.white,
               fontSize: 16.0,
