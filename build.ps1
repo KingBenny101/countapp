5e5795fa917ef6e08fa0e786462154dc23a1695c
KingBenny101 <47111832+KingBenny101@users.noreply.github.com>
2025-01-05 20:28:12 +0530

    Refactored code

--- build.ps1
+++ build.ps1
@@ -3,24 +3,22 @@ $ANDROID_BUILD = "build\app\outputs\flutter-apk\app-release.apk"
 $WINDOWS_BUILD = "build\windows\x64\runner\Release\countapp.msix"
 
 
-function CreatePlatform {
+function GenerateEnvironment {
+    Write-Host "`nInstalling dependencies..."
+    flutter pub get
     Write-Host "`nCreating Android platform..."
     flutter create --platforms=android --android-language=java .
     Write-Host "`nCreating Windows platform..."
     flutter create --platforms=windows .
     Write-Host "`nRemoving Test folder..."
     Remove-Item "test" -Force -Recurse -ErrorAction SilentlyContinue 
-   
-}
-
-function Generate {
     Write-Host "`nRunning package_rename..."
     dart run package_rename
     Write-Host "`nRunning flutter_launcher_icons..."
     dart run flutter_launcher_icons:main
 }
 
-function CleanRelease {
+function CleanEnvironment {
     if (Test-Path $RELEASE_FOLDER) {
         Remove-Item "$RELEASE_FOLDER\*" -Force -ErrorAction SilentlyContinue
         Write-Host "`nCleaned release folder."
@@ -28,25 +26,26 @@ function CleanRelease {
         New-Item -ItemType Directory -Path $RELEASE_FOLDER | Out-Null
         Write-Host "`nCreated release folder."
     }
+
+    Write-Host "`nCleaning build folder..."
+    flutter clean
 }
 
 function BuildAndroid {
     Write-Host "`nBuilding for Android..."
     flutter build apk --release
-}
 
-function BuildWindows {
-    Write-Host "`nBuilding for Windows..."
-    dart run msix:create
-}
-
-function CopyFiles {
     if (Test-Path $ANDROID_BUILD) {
         Copy-Item $ANDROID_BUILD -Destination $RELEASE_FOLDER
         Write-Host "`nAPK file copied successfully."
     } else {
         Write-Host "`nAPK file not found at $ANDROID_BUILD"
     }
+}
+
+function BuildWindows {
+    Write-Host "`nBuilding for Windows..."
+    dart run msix:create
 
     if (Test-Path $WINDOWS_BUILD) {
         Copy-Item $WINDOWS_BUILD -Destination $RELEASE_FOLDER
@@ -57,23 +56,19 @@ function CopyFiles {
 }
 
 function All {
-    CreatePlatform
-    Generate
-    CleanRelease
+    GenerateEnvironment
+    CleanEnvironment
     BuildAndroid
     BuildWindows
-    CopyFiles
 }
 
 $task = $args[0] 
 
 switch ($task) {
-    "create_platform" { CreatePlatform }
     "generate" { Generate }
-    "clean_release" { CleanRelease }
+    "clean" { CleanRelease }
     "build_android" { BuildAndroid }
     "build_windows" { BuildWindows }
-    "copy_files" { CopyFiles }
     "all" { All }
-    default { Write-Host "Invalid task. Available tasks: create_platform, generate, clean_release, build_android, build_windows, copy_files, all" }
+    default { Write-Host "Invalid task. Available tasks: generate, clean, build_android, build_windows, all" }
 }
