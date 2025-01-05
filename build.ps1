1c463ea6f04a9f4d0a9c13f7b20daf77509256a9
KingBenny101 <47111832+KingBenny101@users.noreply.github.com>
2025-01-05 19:49:17 +0530

    Switched to ps1 from Makefile

---
+++ build.ps1
@@ -0,0 +1,67 @@
+$RELEASE_FOLDER = "release"
+$ANDROID_BUILD = "build\app\outputs\flutter-apk\app-release.apk"
+$WINDOWS_BUILD = "build\windows\x64\runner\Release\countapp.msix"
+
+
+function Generate {
+    Write-Host "\nRunning package_rename..."
+    dart run package_rename
+    Write-Host "\nRunning flutter_launcher_icons..."
+    dart run flutter_launcher_icons:main
+}
+
+function CleanRelease {
+    if (Test-Path $RELEASE_FOLDER) {
+        Remove-Item "$RELEASE_FOLDER\*" -Force -ErrorAction SilentlyContinue
+        Write-Host "\nCleaned release folder."
+    } else {
+        New-Item -ItemType Directory -Path $RELEASE_FOLDER | Out-Null
+        Write-Host "\nCreated release folder."
+    }
+}
+
+function BuildAndroid {
+    Write-Host "\nBuilding for Android..."
+    flutter build apk --release
+}
+
+function BuildWindows {
+    Write-Host "\nBuilding for Windows..."
+    dart run msix:create
+}
+
+function CopyFiles {
+    if (Test-Path $ANDROID_BUILD) {
+        Copy-Item $ANDROID_BUILD -Destination $RELEASE_FOLDER
+        Write-Host "\nAPK file copied successfully."
+    } else {
+        Write-Host "\nAPK file not found at $ANDROID_BUILD"
+    }
+
+    if (Test-Path $WINDOWS_BUILD) {
+        Copy-Item $WINDOWS_BUILD -Destination $RELEASE_FOLDER
+        Write-Host "\nEXE file copied successfully."
+    } else {
+        Write-Host "\nEXE file not found at $WINDOWS_BUILD"
+    }
+}
+
+function All {
+    Generate
+    CleanRelease
+    BuildAndroid
+    BuildWindows
+    CopyFiles
+}
+
+$task = $args[0] 
+
+switch ($task) {
+    "generate" { Generate }
+    "clean_release" { CleanRelease }
+    "build_android" { BuildAndroid }
+    "build_windows" { BuildWindows }
+    "copy_files" { CopyFiles }
+    "all" { All }
+    default { Write-Host "Invalid task. Available tasks: generate, clean_release, build_android, build_windows, copy_files, all" }
+}
