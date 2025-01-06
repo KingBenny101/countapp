76b538c1d48f20bd019f639e7df2c407e7ee0796
KingBenny101 <47111832+KingBenny101@users.noreply.github.com>
2025-01-06 16:53:14 +0530

    Added permission check

--- lib/utils/files.dart
+++ lib/utils/files.dart
@@ -3,15 +3,38 @@ import "dart:io";
 
 import "package:countapp/models/counter_model.dart";
 import "package:countapp/providers/counter_provider.dart";
+import "package:countapp/utils/permissions.dart";
+import "package:flutter/material.dart";
 import "package:hive_ce/hive.dart";
+import "package:toastification/toastification.dart";
 
 Future<void> exportJSON(String exportFilePath) async {
-  final box = await Hive.openBox<Counter>("countersBox");
-  final file = File(exportFilePath);
-  final events = box.values.toList();
-  final jsonEvents = json.encode(events);
+  bool hasPermission = await checkAndRequestStoragePermission();
 
-  await file.writeAsString(jsonEvents);
+  if (hasPermission) {
+    final box = await Hive.openBox<Counter>("countersBox");
+    final file = File(exportFilePath);
+    final events = box.values.toList();
+    final jsonEvents = json.encode(events);
+
+    await file.writeAsString(jsonEvents);
+
+    toastification.show(
+      type: ToastificationType.success,
+      alignment: Alignment.bottomCenter,
+      style: ToastificationStyle.simple,
+      title: const Text("Counters Exported Successfully!"),
+      autoCloseDuration: const Duration(seconds: 5),
+    );
+  } else {
+    toastification.show(
+      type: ToastificationType.error,
+      alignment: Alignment.bottomCenter,
+      style: ToastificationStyle.simple,
+      title: const Text("No permission!"),
+      autoCloseDuration: const Duration(seconds: 5),
+    );
+  }
 }
 
 Future<void> importJSON(
