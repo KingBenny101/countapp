6279113baa6f68a806be2e5f34864e81f9a67dea
KingBenny101 <47111832+KingBenny101@users.noreply.github.com>
2025-01-23 20:30:58 +0530

    Move generateStatsWidgets function to widgets utility file

--- lib/utils/widgets.dart
+++ lib/utils/widgets.dart
@@ -52,3 +52,28 @@ Widget buildCustomListTile(DateTime date) {
     ],
   );
 }
+
+List<Widget> generateStatsWidgets(List<dynamic> stats) {
+  final stats0 = stats[0] as double;
+  final stats1 = stats[1] as String;
+  final stats2 = stats[2] as int;
+  final stats3 = stats[3] as String;
+  final stats4 = stats[4] as double;
+  final stats5 = stats[5] as double;
+  final stats6 = stats[6] as String;
+  final stats8 = stats[8] as double;
+
+  return [
+    buildInfoCard("Average Updates per Day", stats0.toStringAsFixed(2)),
+    buildInfoCard("Most Updates Day", stats1),
+    buildInfoCard("Most Updates Count", stats2.toString()),
+    buildInfoCard("Time with the Most Updates", stats3),
+    buildInfoCard("Most Updates during", stats6),
+    buildInfoCard(
+      "Average over the Last 7 Days",
+      stats4.toStringAsFixed(2),
+    ),
+    buildInfoCard("Average over the Last 30 Days", stats8.toStringAsFixed(2)),
+    buildInfoCard("Days with No Updates", "${stats5.toStringAsFixed(2)}%"),
+  ];
+}
