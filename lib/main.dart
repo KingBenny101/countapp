22260234ed65174d8ea00f464eec350ba2867e9c
KingBenny101 <47111832+KingBenny101@users.noreply.github.com>
2025-02-23 14:13:28 +0530

    Add new routes for AboutPage, GuidePage, and UpdatePage in MainApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import "package:countapp/models/counter_model.dart";
 import "package:countapp/providers/counter_provider.dart";
+import "package:countapp/screens/about_page.dart";
+import "package:countapp/screens/guide_page.dart";
 import "package:countapp/screens/home_page.dart";
 import "package:countapp/screens/options_page.dart";
+import "package:countapp/screens/update_page.dart";
 import "package:countapp/theme/theme_notifier.dart";
 import "package:flutter/material.dart";
 import "package:hive_ce_flutter/hive_flutter.dart";
@@ -44,7 +47,10 @@ class MainApp extends StatelessWidget {
             themeMode: themeNotifier.themeMode,
             home: const HomePage(),
             routes: {
+              "/updates" : (context) => const UpdatePage(),
               "/options": (context) => const OptionsPage(),
+              "/guide" : (context) => const GuidePage(),
+              "/about" : (context) => const AboutPage(),
             },
           ),
         );
