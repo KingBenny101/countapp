6e57e803ef3cf91ec16c4390e9cd49b03ff58e38
KingBenny101 <47111832+KingBenny101@users.noreply.github.com>
2026-01-17 21:24:22 +0530

    Refactor imports and improve leaderboard UI with color coding and text contrast

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import "package:countapp/counters/series_counter/series_counter.dart";
 import "package:countapp/counters/tap_counter/tap_counter.dart";
 import "package:countapp/models/counter_model.dart";
+import "package:countapp/models/leaderboard.dart";
 import "package:countapp/providers/counter_provider.dart";
 import "package:countapp/screens/about_page.dart";
 import "package:countapp/screens/home_page.dart";
 import "package:countapp/screens/options_page.dart";
 import "package:countapp/screens/update_page.dart";
-import "package:countapp/models/leaderboard.dart";
 import "package:countapp/theme/theme_notifier.dart";
 import "package:countapp/utils/constants.dart";
 import "package:countapp/utils/migration.dart";
